a3ef8eb8ab3869ef02b25127ff52cdb5677d3400
Ramon S Antonio <monsillva@gmail.com>
2026-05-17 11:32:43 -0300

    Menu Mobile

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
-let btnMenu = document.getElementById('abrir')
+let btnMenu = document.getElementById('button-abrir') 
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('button-abrir')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('button-abrir')
+})
+
